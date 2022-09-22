c206ffcf221065d022585097b518750dee5cf89c
GavinsMJ <gavinsmaragia@gmail.com>
2022-09-22 10:18:58 +0300

    init

---
+++ MultiWiFiConnect/MultiWiFiConnect.ino
@@ -0,0 +1,19 @@
+/*
+Author: Gavins Maragia 
+GITHUB: https://github.com/GavinsMJ
+*/
+
+#define DEBUG_Wifi_multi 1        //set 1 for debug serial statements
+
+#include "definitions.h" // place your wifi networks in this file ;-)
+
+void setup(){
+
+ bool is_wifi_connected = WiFiConnect::init_wifi();
+ 
+}
+
+void loop(){
+ //do cool stuff here
+
+}
